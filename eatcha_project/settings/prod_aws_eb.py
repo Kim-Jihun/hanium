34580980fe56881dfebdcd7af80a311c6cc1d921
Jeong Seung Hwan <tmdghks5466@naver.com>
2017-08-27 02:42:28 +0900

    RDS/S3 연결 설정 추가

--- eatcha_project/settings/prod_aws_eb.py
+++ eatcha_project/settings/prod_aws_eb.py
@@ -21,8 +21,8 @@ DATABASES = {
 INSTALLED_APPS += ['storages']
 # django-storages 앱 의존성 추가
 # 기본 static/media 저장소를 django-storages로 변경
-STATICFILES_STORAGE = 'helloeb3.storages.StaticS3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'helloeb3.storages.MediaS3Boto3Storage'
+STATICFILES_STORAGE = 'eatcha_project.storages.StaticS3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'eatcha_project.storages.MediaS3Boto3Storage'
 # S3 파일 관리에 필요한 최소 설정
 # 소스코드에 설정정보를 남기지마세요. 환경변수를 통한 설정 추천
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
--- eatcha_project/wsgi.py
+++ eatcha_project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eatcha_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eatcha_project.settings.prod_aws_eb")
 
 application = get_wsgi_application()
