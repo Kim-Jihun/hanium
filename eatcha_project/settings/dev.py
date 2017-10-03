e0a3ccf42e3e51fd29ead239cc5f28f6abdf102d
Kim-Jihun <ssamji0529@naver.com>
2017-10-03 20:20:14 +0900

    소셜로그인, 회원가입, 로그인 메커니즘 수정

--- eatcha_project/settings/dev.py
+++ eatcha_project/settings/dev.py
@@ -1,10 +1,14 @@
 from .commons import *
 
-#DJANGO DEGUB TOOLBAR 설
+#DJANGO DEGUB TOOLBAR 설치
 INSTALLED_APPS += ['debug_toolbar']
 MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
 INTERNAL_IPS = ['127.0.0.1']
 
+
+DEBUG = True
+# midify
+ALLOWED_HOSTS = ["*"] # 서비스 주소
 
 
 DATABASES = {
--- eatcha_project/settings/prod.py
+++ eatcha_project/settings/prod.py
@@ -32,8 +32,8 @@ INSTALLED_APPS += ['storages'] # django-storages 앱 의존성 추가
 STATICFILES_STORAGE = 'eatcha_project.storages.StaticS3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'eatcha_project.storages.MediaS3Boto3Storage'
 
-STATIC_URL = 'https://s3.ap-northeast-2.amazonaws.com/eatcha/'
-MEDIA_URL = 'https://s3.ap-northeast-2.amazonaws.com/eatcha/'
+STATIC_URL = 'https://s3.ap-northeast-2.amazonaws.com/eatcha/static/'
+MEDIA_URL = 'https://s3.ap-northeast-2.amazonaws.com/eatcha/media/'
 
 
 # S3 파일 관리에 필요한 최소 설정
