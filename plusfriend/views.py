231f3b565dd70cf19418c11b98ce79f7a0c3343b
Kim-Jihun <ssamji0529@naver.com>
2017-10-12 21:05:08 +0900

    사소한 오류수정

--- plusfriend/views.py
+++ plusfriend/views.py
@@ -7,8 +7,34 @@ from django.http import JsonResponse
 @bot
 def on_init(request):
 
-    return {'type': 'buttons', 'buttons': ['장소', '메뉴', '가격', '왜 안되나 아무거나 시험']}
-    #return {'type': 'text'}
+    return {
+            'message': {
+                'text': 'Eatcha입니다! 맛집검색시 장소, 메뉴, 가격으로 검색해 주세요',
+                'photo': {
+                    #"url": 'https://s3.ap-northeast-2.amazonaws.com/eatcha' + response.image.url,
+                    "url": 'http://blogfiles1.naver.net/20140806_24/eat_korea_1407302198842EXh5b_JPEG/K-2.jpg',
+                    "width": 640,
+                    "height": 480,
+                },
+
+                "message_button": {
+                    "label": "상세 url로 이동",
+                    "url": 'http://production.g3g2me2gp2.ap-northeast-2.elasticbeanstalk.com/shop/home/'
+                    #"url": 'www.naver.com'
+                },
+
+            },
+
+
+            "keyboard": {
+                "type": "text",
+                "buttons": [
+                  "메뉴",
+                  "가격",
+                  "장소"
+                ]
+             }
+        }
 
 
 @bot
@@ -22,16 +48,22 @@ def on_message(request):
 
     if '장소' in content:
         return {'type': 'buttons', 'buttons': ['서울대입구', '신촌', '왕십리']}
-    elif '신' in content:
-        qs = Post.objects.filter(tag_set__location__icontains='신촌')
-        ordered_query = qs.order_by('-score')
-        response = ordered_query[0]
 
-    elif '왕십리' in content:
+    elif content in ['서울대입구', '신촌', '왕십리'] :
+        location = content
+        return {'type': 'buttons', 'buttons': ['메뉴(#을 앞에 붙여주세)','가격']}
+        #qs = Post.objects.filter(tag_set__location__icontains='신촌')
+        #ordered_query = qs.order_by('-score')
+        #response = ordered_query[0]
 
-        qs = Post.objects.filter(tag_set__location__icontains='왕십리')
-        ordered_query = qs.order_by('-score')
-        response = ordered_query[0]
+    elif '메뉴' in content:
+
+        #qs = Post.objects.filter(tag_set__location__icontains='왕십리')
+        #ordered_query = qs.order_by('-score')
+        #response = ordered_query[0]
+        return {'type': 'text', 'buttons': ['가격']}
+
+
 
     else:
         response='지원하는 답변이 아닙니다.'
