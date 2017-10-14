dce247b081439b04f49a3d2b8c621abe956d54d7
Kim-Jihun <ssamji0529@naver.com>
2017-10-14 09:32:52 +0900

    아 혼자 다하네 ㅅㅂ

--- shop/utils/crawling_restaurant/crawling.py
+++ shop/utils/crawling_restaurant/crawling.py
@@ -12,7 +12,7 @@ def crawling_data(q):
     url = "https://m.store.naver.com/sogum/api/businesses"
 
 
-    for k in range(0, 1):
+    for k in range(0, 5):
         params = {
             'query': q,
             'start' : count,
